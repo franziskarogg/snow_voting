e2c5a8ba02c6fb2397ac9fca0f56e05ca8f36679
Franziska Rogg <Franzi@klinikums-MacBook-Pro.local>
2026-05-14 11:49:25 +0200

    plots, tables, descriptives

--- analyses/analysis.R
+++ analyses/analysis.R
@@ -116,45 +116,103 @@ summary(r4)
 
 
 
-
 #-------------------------------------------------------------------------------
 #Modelsummaries 
 #-------------------------------------------------------------------------------
 
+library(modelsummary)
+library(sandwich)
 
+# Table 1 - Anomaly
+modelsummary(list("OLS FPÖ" = mr1, 
+                  "FE FPÖ" = m1, 
+                  "OLS Greens" = mr3, 
+                  "FE Greens" = m3),
+             stars = TRUE,
+             vcov = list("HC1", ~district_id, "HC1", ~district_id),
+             gof_map = c("nobs", "r.squared", "adj.r.squared"),
+             output = "./tables/table_anomaly.tex")
 
+# Table 2 - Mean Snow
+modelsummary(list("OLS FPÖ" = mr2, 
+                  "FE FPÖ" = m2, 
+                  "OLS Greens" = mr4, 
+                  "FE Greens" = m4),
+             stars = TRUE,
+             vcov = list("HC1", ~district_id, "HC1", ~district_id),
+             gof_map = c("nobs", "r.squared", "adj.r.squared"),
+             output = "./tables/table_mean.tex")
+
+# Table 3 - Interaction - Heterogeneity across Altitude 
+modelsummary(list("OLS FPÖ" = mr2b, 
+                  "FE FPÖ" = m2b),
+             stars = TRUE,
+             vcov = list("HC1", ~district_id),
+             gof_map = c("nobs", "r.squared", "adj.r.squared"),
+             output = "./tables/table_interaction.tex")
+
+# Table 4 - Robustness
+modelsummary(list("OLS FPÖ" = r3, 
+                  "FE FPÖ" = r1, 
+                  "OLS Greens" = r4, 
+                  "FE Greens" = r2),
+             stars = TRUE,
+             vcov = list("HC1", ~district_id, "HC1", ~district_id),
+             gof_map = c("nobs", "r.squared", "adj.r.squared"),
+             output = "./tables/table_robustness.tex")
 
 
 #-------------------------------------------------------------------------------
 #Figures 
 #-------------------------------------------------------------------------------
+plotformat = theme(
+  plot.background = element_rect(fill = "white", color = NA),
+  legend.key.size = unit(0.4, "cm"),
+  legend.text = element_text(size = 7),
+  legend.title = element_text(size = 8)
+)
+
 
 #Plotting mean snow-depth across municipalities, ANOMALY
-p1 = ggplot(snow_sf_mean) +
-  geom_sf(aes(fill = anomaly),
-          color = NA) +
+p1 =  ggplot(snow_sf_mean) +
+  geom_sf(aes(fill = anomaly), color = NA) +
   scale_fill_gradient2(
-    low = "#08306b", 
-    mid = "#6baed6",
-    high = "#deebf7",
-    midpoint = 0,
-    name = "Deviation of Snow Depth 23/24 from Historical Mean"
+    low = "#a50f15",    # dark red = negative anomaly (less snow than usual)
+    mid = "#f0f0f0",    # white = zero (normal)
+    high = "#08306b",   # dark blue = positive anomaly (more snow than usual)
+    midpoint = 0, 
+    name = "Anomaly (m)"
   ) +
-  theme_void() 
+  theme_void() + plotformat
+
+ggsave("./plots/snow_anomaly_map.png", plot = p1, width = 5, height = 3, dpi = 300)
 
 #MEAN 23/24
 p2 = ggplot(snow_sf_mean) +
-  geom_sf(aes(fill = mean_2324),
-          color = NA) +
+  geom_sf(aes(fill = mean_2324), color = NA) +
   scale_fill_gradient2(
-    low = "#08306b", 
-    high = "#deebf7",
-    name = "Mean Snow Depth Winter 23/24 (m) "
+    low = "#08306b", mid = "#6baed6", high = "#deebf7",
+    midpoint = 0, name = "Mean Snow Depth 23/24 (m)"
   ) +
-  theme_void() 
+  theme_void() + plotformat
 
-ggsave("../plots/snow_anomaly_map.png", plot = p1, width = 5, height = 3, dpi = 300)
-ggsave("../plots/snow_mean_map.png", plot = p2, width = 5, height = 3, dpi = 300)
+ggsave("./plots/snow_mean_map.png", plot = p2, width = 5, height = 3, dpi = 300)
+
+#Vote Shares of the National Election
+p7 = df_mean %>%
+  summarise(
+    FPÖ = mean(fpö_share, na.rm = TRUE),
+    Greens = mean(greens_share, na.rm = TRUE)
+  ) %>%
+  pivot_longer(everything(), names_to = "party", values_to = "share") %>%
+  ggplot(aes(x = party, y = share, fill = party)) +
+  geom_bar(stat = "identity", width = 0.5) +
+  scale_fill_manual(values = c("FPÖ" = "#003f7f", "Greens" = "#006d2c")) +
+  theme_minimal() +
+  labs(x = "", y = "Average Vote Share per Municipality (%)") +
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "white", color = NA))
+ggsave("./plots/vote_shares_bar.png", plot = p7, width = 4, height = 3, dpi = 300)
 
 ##Plotting vote-shares across municipalities
 munic24_votes = munic24 %>% 
@@ -164,42 +222,49 @@ summary(munic24_votes$fpö_share)
 summary(munic24_votes$greens_share)
 
 #FPÖ Map
-p5 = ggplot(munic24_votes) + 
-  geom_sf(aes(fill = fpö_share, color = NA) + 
-            scale_fill_gradient(
-              low = "#fee5d9", 
-              high = "#a50f15", 
-              name = "FPÖ Share (%)"
-                ) + 
-            theme_void ())
+p5 = ggplot(munic24_votes) +
+  geom_sf(aes(fill = fpö_share), color = NA) +
+  scale_fill_gradient(low = "#fee5d9", high = "#a50f15", name = "FPÖ Share (%)") +
+  theme_void() + plotformat
           
-p6 = ggplot(munic24_votes) + 
-  geom_sf(aes(fill = greens_share), color = NA + 
-            scale_fill_gradient(
-              low = "#e5f5e0", 
-              high = "#006d2c", 
-              name = "Greens Share (%)"
-            ) + 
-            theme_void())
+p6 = ggplot(munic24_votes) +
+  geom_sf(aes(fill = greens_share), color = NA) +
+  scale_fill_gradient(low = "#e5f5e0", high = "#006d2c", name = "Greens Share (%)") +
+  theme_void() + plotformat
           
-ggsave("../plots/fpö_share_map.png", plot = p5, width = 5, height = 3, dpi = 300)
-ggsave("../plots/greens_share_map.png", plot = p6, width = 5, height = 3, dpi = 300)
+ggsave("./plots/fpö_share_map.png", plot = p5, width = 5, height = 3, dpi = 300)
+ggsave("./plots/greens_share_map.png", plot = p6, width = 5, height = 3, dpi = 300)
 
-#Plots for vote shares in regions wiht high/low mean snow-depth 
-p3 = ggplot(df_mean %>% filter(snow_group != "Middle 50%"), 
+#Plots for vote shares in regions with high/low mean snow-depth 
+p3 = ggplot(df_mean  %>% filter(snow_group != "Middle 50%"),
             aes(x = snow_group, y = fpö_share, fill = snow_group)) +
   geom_boxplot() +
   scale_fill_manual(values = c("Bottom 25%" = "#08306b", "Top 25%" = "#deebf7")) +
   theme_minimal() +
   labs(x = "Grouped Means of Snow Depth 23/24", y = "FPÖ Share (%)") +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "white", color = NA))
 
-p4 = ggplot(df_mean, aes(x = mean_2324, y = fpö_share, color = region)) +
-  geom_point(alpha = 0.3) +
-  geom_smooth(method = "loess") +  # flexible trend, no linearity assumption
+
+#p4 = ggplot(df_mean, aes(x = mean_2324, y = fpö_share, color = region)) +
+ # geom_point(alpha = 0.3) +
+ # geom_smooth(method = "loess") +  # flexible trend, no linearity assumption
+  #theme_minimal() +
+  #labs(x = "Mean Snow Depth 2023/24", y = "FPÖ Share (%)")
+
+ggsave("./plots/boxplot_fpö_snow.png", plot = p3, width = 5, height = 3, dpi = 300)
+#ggsave("./plots/scatter_fpö_snow.png", plot = p4, width = 5, height = 3, dpi = 300)
+
+p7 = df_mean %>%
+  summarise(FPÖ = mean(fpö_share, na.rm = TRUE),
+            Greens = mean(greens_share, na.rm = TRUE)) %>%
+  pivot_longer(everything(), names_to = "party", values_to = "share") %>%
+  ggplot(aes(x = party, y = share, fill = party)) +
+  geom_bar(stat = "identity", width = 0.5) +
+  scale_fill_manual(values = c("FPÖ" = "#a50f15", "Greens" = "#006d2c")) +
   theme_minimal() +
-  labs(x = "Mean Snow Depth 2023/24", y = "FPÖ Share (%)")
+  labs(x = "", y = "Mean Vote Share (%)") +
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "white", color = NA))
 
-ggsave("../plots/boxplot_fpö_snow.png", plot = p3, width = 5, height = 3, dpi = 300)
-ggsave("../plots/scatter_fpö_snow.png", plot = p4, width = 5, height = 3, dpi = 300)
-
+ggsave("./plots/vote_shares_bar.png", plot = p7, width = 4, height = 3, dpi = 300)
